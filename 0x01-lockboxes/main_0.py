f25525a2544c1f317c0039b7c0c11f3360855a57
Ngozi Agomuonso <ngozi.agomuonso@gmail.com>
2024-05-02 21:20:57 +0100

    Unlock Update

--- 0x01-lockboxes/main_0.py
+++ 0x01-lockboxes/main_0.py
@@ -2,11 +2,11 @@
 
 canUnlockAll = __import__('0-lockboxes').canUnlockAll
 
-boxes1 = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes1))
+boxes = [[1], [2], [3], [4], []]
+print(canUnlockAll(boxes))
 
-boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes2))
+boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes))
 
-boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes3))
+boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes))
