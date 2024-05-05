73137d52f106ab43c3f9cc31ef6bf929208da1dd
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-05 15:32:55 +0100

    unlock locked boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+"""
+Module to determine if all boxes can be unlocked.
+"""
 
 
 def look_next_opened_box(opened_boxes):
-    """Looks for the next opened box
+    """
+    Looks for the next opened box.
+
     Args:
-        opened_boxes (dict): Dictionary which contains boxes already opened
+        opened_boxes (dict): Dictionary containing opened boxes.
+
     Returns:
-        list: List with the keys contained in the opened box
+        list: List with the keys contained in the opened box.
     """
     for index, box in opened_boxes.items():
         if box.get('status') == 'opened':
@@ -15,12 +21,15 @@ def look_next_opened_box(opened_boxes):
     return None
 
 
-def canUnlockAll(boxes):
-    """Check if all boxes can be opened
+def can_unlock_all(boxes):
+    """
+    Check if all boxes can be opened.
+
     Args:
-        boxes (list): List which contain all the boxes with the keys
+        boxes (list): List containing all the boxes with the keys.
+
     Returns:
-        bool: True if all boxes can be opened, otherwise, False
+        bool: True if all boxes can be opened, otherwise False.
     """
     if len(boxes) <= 1 or boxes == [[]]:
         return True
@@ -56,8 +65,10 @@ def canUnlockAll(boxes):
 
 
 def main():
-    """Entry point"""
-    canUnlockAll([[]])
+    """
+    Entry point of the script.
+    """
+    can_unlock_all([[]])
 
 
 if __name__ == '__main__':
