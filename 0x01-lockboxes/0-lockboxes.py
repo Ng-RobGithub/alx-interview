aa5e248dd42e38d2a996afb61772f0253a267c8e
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-05 15:54:16 +0100

    unlock locked boxes3

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,72 +1,34 @@
 #!/usr/bin/python3
-"""
-Script to check if all boxes can be opened.
-"""
-
-def look_next_opened_box(opened_boxes):
-    """
-    Looks for the next opened box.
-
-    Args:
-        opened_boxes (dict): Dictionary containing opened boxes.
-
-    Returns:
-        list: List with the keys contained in the opened box.
-    """
-    for index, box in opened_boxes.items():
-        if box.get('status') == 'opened':
-            box['status'] = 'opened/checked'
-            return box.get('keys')
-    return None
+"""Determines if all boxes can be opened from a list of lists."""
 
 
-def can_unlock_all(boxes):
-    """
-    Check if all boxes can be opened.
+def can_unlock_all(boxes=[]):
+    """Returns True if all boxes in boxes can be opened."""
+    if not boxes:
+        return False
 
-    Args:
-        boxes (list): List containing all the boxes with the keys.
+    keys = {0}
+    for box_id, box in enumerate(boxes):
+        for key in box:
+            if key < len(boxes) and key != box_id:
+                keys.add(key)
 
-    Returns:
-        bool: True if all boxes can be opened, otherwise, False.
-    """
-    if len(boxes) <= 1 or boxes == [[]]:
-        return True
+    if len(keys) != len(boxes):
+        return False
 
-    aux = {}
-    while True:
-        if len(aux) == 0:
-            aux[0] = {
-                'status': 'opened',
-                'keys': boxes[0],
-            }
-        keys = look_next_opened_box(aux)
-        if keys:
-            for key in keys:
-                try:
-                    if aux.get(key) and aux.get(key).get('status') \
-                       == 'opened/checked':
-                        continue
-                    aux[key] = {
-                        'status': 'opened',
-                        'keys': boxes[key]
-                    }
-                except (KeyError, IndexError):
-                    continue
-        elif 'opened' in [box.get('status') for box in aux.values()]:
-            continue
-        elif len(aux) == len(boxes):
-            break
-        else:
-            return False
-
-    return len(aux) == len(boxes)
-
-
-def main():
-    """Entry point."""
-    can_unlock_all([[]])
+    return True
 
 
 if __name__ == '__main__':
-    main()
+    # Test cases
+    boxes1 = [[1, 3], [2], [3, 0], [1, 2, 3]]
+    print(can_unlock_all(boxes1))
+
+    boxes2 = [[1], [2], [3], [4], []]
+    print(can_unlock_all(boxes2))
+
+    boxes3 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(can_unlock_all(boxes3))
+
+    boxes4 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(can_unlock_all(boxes4))
