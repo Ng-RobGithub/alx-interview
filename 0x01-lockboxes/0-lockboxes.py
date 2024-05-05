4898f5d5636e0a856bd0b468a159e0dceb2af65c
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-05 17:09:14 +0100

    unlock lock boxes2

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python3
 
 def canUnlockAll(boxes):
+    """
+    Determines if all the boxes can be opened.
+
+    Args:
+        boxes (list): A list of lists representing the boxes and their keys.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
+
+    # If no boxes are provided, return False
     if not boxes:
         return False
 
+    # Initialize a set to store keys and a set to store unlocked boxes
     keys = set(boxes[0])
     unlocked = set([0])
 
+    # Perform a BFS traversal to unlock all possible boxes
     while keys:
         key = keys.pop()
         if key < len(boxes) and key not in unlocked:
             keys.update(boxes[key])
             unlocked.add(key)
 
+    # Return True if all boxes are unlocked, otherwise False
     return len(unlocked) == len(boxes)
