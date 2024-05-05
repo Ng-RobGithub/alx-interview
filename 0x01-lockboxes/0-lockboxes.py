1ae3c0469bcf74f03cc3a1e9eb4eea7c6fd3e9b6
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-05 20:22:54 +0100

    unlock the locked

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-A Python module that determines if all boxes can be opened from a list of
-lists.
+A Python module to determine if all boxes can be opened from a list of lists.
 """
 
 
@@ -10,7 +9,7 @@ def can_unlock_all(boxes=[]):
     Determine if all boxes in the list can be opened.
 
     Args:
-        boxes (list): A list of lists representing the boxes and their keys.
+        boxes (list): List of lists containing keys in each box.
 
     Returns:
         bool: True if all boxes can be opened, False otherwise.
@@ -31,20 +30,19 @@ def can_unlock_all(boxes=[]):
 
 
 if __name__ == '__main__':
-    # Test cases
-    boxes1 = [
+    boxes = [
         [1, 3],
         [2],
         [3, 0],
         [1, 2, 3],
     ]
-    print(can_unlock_all(boxes1))
+    print(can_unlock_all(boxes))
 
-    boxes2 = [[1], [2], [3], [4], []]
-    print(can_unlock_all(boxes2))
+    boxes = [[1], [2], [3], [4], []]
+    print(can_unlock_all(boxes))
 
-    boxes3 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(can_unlock_all(boxes3))
+    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(can_unlock_all(boxes))
 
-    boxes4 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(can_unlock_all(boxes4))
+    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(can_unlock_all(boxes))
