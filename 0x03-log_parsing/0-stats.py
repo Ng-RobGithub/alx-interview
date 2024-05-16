a8aec70573567e6f500cb90c43da858e6df46a92
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-17 00:22:48 +0100

    Log parsing2

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,48 +1,44 @@
 #!/usr/bin/python3
+"""Log Parser"""
 import sys
 
 
-def print_statistics(total_size, status_counts):
-    print("File size: {}".format(total_size))
-    for code, count in sorted(status_counts.items()):
-        if count > 0:
-            print("{}: {}".format(code, count))
+if __name__ == '__main__':
+    file_size = [0]
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
 
+    def print_stats():
+        """ Print statistics """
+        print('File size: {}'.format(file_size[0]))
+        for key in sorted(status_codes.keys()):
+            if status_codes[key]:
+                print('{}: {}'.format(key, status_codes[key]))
 
-def parse_line(line, status_counts):
-    parts = line.split()
-    if len(parts) >= 9:
-        status_code = parts[8]
+    def parse_line(line):
+        """ Checks the line for matches """
         try:
-            status_code = int(status_code)
-            if status_code in status_counts:
-                status_counts[status_code] += 1
-        except ValueError:
+            line = line[:-1]
+            word = line.split(' ')
+            # File size is last parameter on stdout
+            file_size[0] += int(word[-1])
+            # Status code comes before file size
+            status_code = int(word[-2])
+            # Move through dictionary of status codes
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except BaseException:
             pass
 
-
-def main():
-    total_size = 0
-    status_counts = {
-        200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-    line_count = 0
-
+    linenum = 1
     try:
         for line in sys.stdin:
-            line = line.strip()
-            parse_line(line, status_counts)
-            parts = line.split()
-            if len(parts) >= 10:
-                try:
-                    total_size += int(parts[9])
-                except ValueError:
-                    pass
-            line_count += 1
-            if line_count % 10 == 0:
-                print_statistics(total_size, status_counts)
+            parse_line(line)
+            """ print after every 10 lines """
+            if linenum % 10 == 0:
+                print_stats()
+            linenum += 1
     except KeyboardInterrupt:
-        print_statistics(total_size, status_counts)
-
-
-if __name__ == "__main__":
-    main()
+        print_stats()
+        raise
+    print_stats()
