c2fa1f8ae37f5834a434c7b1fabe2265311bf6d4
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-19 00:07:47 +0100

    Log Pasrsing update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -9,28 +9,29 @@ if __name__ == '__main__':
                     403: 0, 404: 0, 405: 0, 500: 0}
 
 
-    def print_stats():
-        """ Print statistics """
-        print('File size: {}'.format(file_size[0]))
-        for key in sorted(status_codes.keys()):
-            if status_codes[key]:
-                print('{}: {}'.format(key, status_codes[key]))
+def print_stats():
+    """ Print statistics """
+    print('File size: {}'.format(file_size[0]))
+    for key in sorted(status_codes.keys()):
+        if status_codes[key]:
+            print('{}: {}'.format(key, status_codes[key]))
 
 
-    def parse_line(line):
-        """ Checks the line for matches """
-        try:
-            line = line[:-1]
-            word = line.split(' ')
-            # File size is last parameter on stdout
-            file_size[0] += int(word[-1])
-            # Status code comes before file size
-            status_code = int(word[-2])
-            # Move through dictionary of status codes
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-        except BaseException:
-            pass
+def parse_line(line):
+    """ Checks the line for matches """
+    try:
+        line = line[:-1]
+        word = line.split(' ')
+        # File size is last parameter on stdout
+        file_size[0] += int(word[-1])
+        # Status code comes before file size
+        status_code = int(word[-2])
+        # Move through dictionary of status codes
+        if status_code in status_codes:
+            status_codes[status_code] += 1
+
+    except BaseException:
+        pass
 
     linenum = 1
     try:
