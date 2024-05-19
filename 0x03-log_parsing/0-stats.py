543e096e777001caf0f2d57e825bbab1ad061c07
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-19 13:48:17 +0100

    Log Pasrsing update2

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,47 +1,49 @@
 #!/usr/bin/python3
 """Log Parser"""
+
 import sys
 
 
-if __name__ == '__main__':
-    file_size = [0]
-    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
-                    403: 0, 404: 0, 405: 0, 500: 0}
-
-
-def print_stats():
-    """ Print statistics """
+def print_stats(file_size, status_codes):
+    """Print statistics."""
     print('File size: {}'.format(file_size[0]))
     for key in sorted(status_codes.keys()):
         if status_codes[key]:
             print('{}: {}'.format(key, status_codes[key]))
 
 
-def parse_line(line):
-    """ Checks the line for matches """
+def parse_line(line, file_size, status_codes):
+    """Parse a single line of input and update metrics."""
     try:
-        line = line[:-1]
-        word = line.split(' ')
-        # File size is last parameter on stdout
-        file_size[0] += int(word[-1])
-        # Status code comes before file size
-        status_code = int(word[-2])
-        # Move through dictionary of status codes
+        parts = line.split()
+        # File size is the last parameter
+        file_size[0] += int(parts[-1])
+        # Status code is the second-to-last parameter
+        status_code = int(parts[-2])
+        # Update status code count if it exists in the dictionary
         if status_code in status_codes:
             status_codes[status_code] += 1
-
-    except BaseException:
+    except (IndexError, ValueError):
         pass
 
-    linenum = 1
+
+def main():
+    file_size = [0]
+    status_codes = {
+        200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    line_num = 0
+
     try:
         for line in sys.stdin:
-            parse_line(line)
-            """ print after every 10 lines """
-            if linenum % 10 == 0:
-                print_stats()
-            linenum += 1
+            parse_line(line, file_size, status_codes)
+            line_num += 1
+            if line_num % 10 == 0:
+                print_stats(file_size, status_codes)
     except KeyboardInterrupt:
-        print_stats()
+        print_stats(file_size, status_codes)
         raise
-    print_stats()
+    print_stats(file_size, status_codes)
+
+
+if __name__ == "__main__":
+    main()
