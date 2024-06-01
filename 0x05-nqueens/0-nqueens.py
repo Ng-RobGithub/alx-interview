3252f477a803ce65338e4847ea14c2f704446d53
Ng-RobGithub <ngrob4real@gmail.com>
2024-06-01 13:53:02 +0100

    Commit N-queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,42 +3,65 @@
 import sys
 
 
-if len(sys.argv) > 2 or len(sys.argv) < 2:
-    print("Usage: nqueens N")
-    exit(1)
-
-if not sys.argv[1].isdigit():
-    print("N must be a number")
-    exit(1)
-
-if int(sys.argv[1]) < 4:
-    print("N must be at least 4")
-    exit(1)
-
-n = int(sys.argv[1])
+def is_safe(board, row, col):
+    """
+    Check if it's safe to place a queen at board[row][col]
+    """
+    for i in range(row):
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
+            return False
+    return True
 
 
-def queens(n, i=0, a=[], b=[], c=[]):
-    """ find possible positions """
-    if i < n:
-        for j in range(n):
-            if j not in a and i + j not in b and i - j not in c:
-                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+def solve_n_queens_util(board, row, n):
+    """
+    Utilizes backtracking to solve the N Queens problem
+    """
+    if row == n:
+        # All queens are placed successfully
+        return True
+
+    for col in range(n):
+        if is_safe(board, row, col):
+            board[row] = col
+            if solve_n_queens_util(board, row + 1, n):
+                return True
+            # Backtrack
+            board[row] = -1
+
+    return False
+
+
+def solve_n_queens(n):
+    """
+    Solves the N Queens problem and prints the solution
+    """
+    board = [-1] * n
+    if solve_n_queens_util(board, 0, n):
+        # Print the solution
+        print_solution(board)
     else:
-        yield a
+        print("No solution exists")
 
 
-def solve(n):
-    """ solve """
-    k = []
-    i = 0
-    for solution in queens(n, 0):
-        for s in solution:
-            k.append([i, s])
-            i += 1
-        print(k)
-        k = []
-        i = 0
+def print_solution(board):
+    """
+    Print the board
+    """
+    n = len(board)
+    for row in range(n):
+        line = ""
+        for col in range(n):
+            if board[row] == col:
+                line += "Q "
+            else:
+                line += ". "
+        print(line)
+    print()
 
 
-solve(n)
+# Example usage
+n = 4
+solve_n_queens(n)
