c0ab011f66eafbce518ec4225f8594da752d1e64
Ng-RobGithub <ngrob4real@gmail.com>
2024-08-06 07:11:54 +0100

    War Characters2

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,29 +1,10 @@
-#!/usr/bin/node
-/**
- * Prints all characters of a Star Wars movie
- * The first positional argument passed is the Movie ID
- * Display one character name per line in the same order
- * as the list in the /films/ endpoint
- */
+import requests
 
-const axios = require('axios');
-const filmNum = process.argv[2];
-const filmURL = `https://swapi-api.hbtn.io/api/films/${filmNum}/`;
+def get_character(character_id):
+    response = requests.get(f"https://swapi.dev/api/people/{character_id}/")
+    return response.json()
 
-const getCharacterNames = async (filmURL) => {
-  try {
-    // Fetch the film data
-    const filmResponse = await axios.get(filmURL);
-    const charURLs = filmResponse.data.characters;
-
-    // Fetch and print each character's name
-    for (const charURL of charURLs) {
-      const charResponse = await axios.get(charURL);
-      console.log(charResponse.data.name);
-    }
-  } catch (error) {
-    console.error('Error:', error.message);
-  }
-};
-
-getCharacterNames(filmURL);
+character = get_character(1)
+print(f"Name: {character['name']}")
+print(f"Height: {character['height']} cm")
+print(f"Mass: {character['mass']} kg")
