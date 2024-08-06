10fbbea06f624efacea15237cd3532a062cf613b
Ng-RobGithub <ngrob4real@gmail.com>
2024-08-06 17:38:54 +0100

    N-Queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,67 +1,44 @@
 #!/usr/bin/python3
-"""
-N Queens Puzzle Solver
-"""
-
+""" N queens """
 import sys
 
 
-def print_solution(board):
-    """
-    Print the solution in the required format.
-    """
-    solution = []
-    for i in range(len(board)):
-        solution.append([i, board[i]])
-    print(solution)
+if len(sys.argv) > 2 or len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    exit(1)
+
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
+
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
+
+n = int(sys.argv[1])
 
 
-def is_safe(board, row, col):
-    """
-    Check if it's safe to place a queen at board[row][col]
-    """
-    for i in range(row):
-        if (board[i] == col or
-                board[i] - i == col - row or
-                board[i] + i == col + row):
-            return False
-    return True
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ find possible positions """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
 
 
-def solve_nqueens(board, row):
-    """
-    Utilizes backtracking to solve the N Queens problem
-    """
-    n = len(board)
-    if row == n:
-        print_solution(board)
-        return
-    for col in range(n):
-        if is_safe(board, row, col):
-            board[row] = col
-            solve_nqueens(board, row + 1)
-            board[row] = -1
+def solve(n):
+    """ solve """
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
 
 
-def main():
-    """
-    Main function to handle input and start solving the problem
-    """
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    board = [-1] * n
-    solve_nqueens(board, 0)
-
-
-if __name__ == "__main__":
-    main()
+solve(n)
