6f5c92ca885e7e9c19334cb738acd94b28a73134
Ng-RobGithub <ngrob4real@gmail.com>
2024-08-06 15:56:13 +0100

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,49 +1,42 @@
 #!/usr/bin/python3
-def sieve_of_eratosthenes(limit):
-    is_prime = [True] * (limit + 1)
-    is_prime[0], is_prime[1] = False, False
-    p = 2
-    while p * p <= limit:
-        if is_prime[p]:
-            for i in range(p * p, limit + 1, p):
-                is_prime[i] = False
-        p += 1
-    return [num for num, prime in enumerate(is_prime) if prime]
+"""0. Prime Game - Maria and Ben are playing a game"""
+
 
 def isWinner(x, nums):
-    if not nums or x < 1:
+    """x - rounds
+    nums - numbers list
+    """
+    if x <= 0 or nums is None:
         return None
-    
-    max_n = max(nums)
-    primes = sieve_of_eratosthenes(max_n)
-    prime_set = set(primes)
-    
-    def play_game(n):
-        primes = [p for p in range(2, n + 1) if p in prime_set]
-        turn = 'Maria'
-        while primes:
-            current_prime = primes.pop(0)
-            primes = [p for p in primes if p % current_prime != 0]
-            turn = 'Ben' if turn == 'Maria' else 'Maria'
-        return 'Ben' if turn == 'Maria' else 'Maria'
-    
-    maria_wins = 0
-    ben_wins = 0
-    
-    for n in nums:
-        winner = play_game(n)
-        if winner == 'Maria':
-            maria_wins += 1
-        else:
-            ben_wins += 1
-    
-    if maria_wins > ben_wins:
-        return 'Maria'
-    elif ben_wins > maria_wins:
-        return 'Ben'
-    else:
+    if x != len(nums):
         return None
 
-if __name__ == "__main__":
-    # Example usage
-    print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
+    ben = 0
+    maria = 0
+
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    for i in range(2, len(a)):
+        rm_multiples(a, i)
+
+    for i in nums:
+        if sum(a[0:i + 1]) % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+    if ben > maria:
+        return "Ben"
+    if maria > ben:
+        return "Maria"
+    return None
+
+
+def rm_multiples(ls, x):
+    """removes multiple
+    of primes
+    """
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break
